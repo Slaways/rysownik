791eaf5cb80341e53e0200fdb77ef1c36d02b9c2
Slaways <tomotp@yahoo.com>
2021-09-11 12:11:27 +0200

    note update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rysownik
+Szkicownik, aktywowany przez najechanie kursorem na wytworzoną siatkę dzięki JS. (JS/CSS/HTML DOM manipulation)
--- js/script.js
+++ js/script.js
@@ -4,11 +4,12 @@ const ereser = document.querySelector('.erese');
 const setnew = document.querySelector('.newGrid');
 let color = "black";
 
-createGrid(10);
+createGrid(10); //Początkowa siatka
 
 ereser.addEventListener('click', ereseGrid);
 setnew.addEventListener('click', newGrid);
 
+//Tworzenie siatki na podstawie podanej liczby (default: 10), dodanie listenerow i przypisanie funkcji kolorowania
 function createGrid(gridNumber){
     let gridSize = gridNumber * gridNumber;
     gridContainer.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
@@ -20,13 +21,19 @@ function createGrid(gridNumber){
     let allGrid = gridContainer.querySelectorAll('div');
     allGrid.forEach(oneGrid => oneGrid.addEventListener('mouseover', colorGrid));
 }
+
+//funkcja kolorowania siatki
 function colorGrid(){
     this.style.backgroundColor = color;
 }
+
+//czyszczenie siatki
 function ereseGrid(){
     let allGrid = gridContainer.querySelectorAll('div');
     allGrid.forEach(oneGrid => oneGrid.style.backgroundColor= "white");
 }
+
+//wartość własna siatki oraz czyszczenie
 function newGrid(){
     let gridSize = prompt("Podaj liczbę określającą wielkość szkicownika (od 1 do 100)");
     if((gridSize<1) || (gridSize>100)){
